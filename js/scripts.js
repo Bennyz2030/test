8c0b436522de3a6c483c5d038498048e9de98073
NIck <nicholasbenzinger@gmail.com>
2021-08-09 15:06:00 -0700

    capitalize first and last letter of sentence
    co-authored by Chynna Lew chynnalew@yahoo.com

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
   let sentence = prompt("Enter a sentence: ");
-let rest = sentence.slice(1);
-let capital = sentence.charAt(0).toUpperCase();
-function output(rest, capital) {
-	return capital + rest;
-}
-  alert(output(rest, capital));
+  let capital = sentence.charAt(0).toUpperCase();
+   let rest = sentence.slice(1, -1);
+  let capital2 = sentence.charAt(sentence.length-1).toUpperCase();
+  function output(capital, rest, capital2) {
+  return capital + rest + capital2;
+  } //This capitalizes the first and last letter of the sentence
+    alert(output(capital, rest, capital2));
   
 }
